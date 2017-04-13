67cb2eb0deeb0e96326629fc7b09246113f1eb08
Sam He <sam@berkerynoyes.corp>
2017-04-13 15:41:53 -0400

    add todo id

--- server.js
+++ server.js
@@ -3,8 +3,45 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+var todos=[{
+    id:1,
+    description:'Meet someone for lunch',
+    completed:false
+},
+{
+    id:2,
+    description:'go to market',
+    completed:false
+},
+           {
+    id:3,
+    description:'feed the dog',
+    completed:true
+}
+];
 app.get('/', function(req,res){
     res.send('Todo API Root');
+});
+
+//GET /todo
+app.get('/todos',function(req,res){
+    res.json(todos);
+})
+
+app.get('/todos/:id',function(req,res){
+   var todoId = parseInt(req.params.id,10);
+    var matchtodo;
+    todos.forEach(function(todo){
+        if(todoId === todo.id)
+            {
+                matchtodo=todo;
+            }});
+    if(matchtodo){
+        res.json(matchtodo)
+    }else{
+        res.status(404)
+    }
+    
 })
 
 app.listen(PORT,function(req,res){
